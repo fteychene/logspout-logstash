a91464d9d44756f4cc957b8b81e257b61dda3438
fteychene <francois.teychene@gmail.com>
2016-05-03 14:40:47 +0200

    Trying something

--- modules.go
+++ modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/fteychene/logspout-logstash"
+	_ "github.com/fteychene/logspout-logstash/adapters/logstash"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/routesapi"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
