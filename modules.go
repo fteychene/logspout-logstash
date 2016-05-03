d953ca55c1a56e6f6390b873ad4d036306366fe4
fteychene <francois.teychene@gmail.com>
2016-05-03 14:33:40 +0200

    Add new adapters to use for logstash network call.

--- modules.go
+++ modules.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	_ "github.com/looplab/logspout-logstash"
-	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/fteychene/logspout-logstash"
 	_ "github.com/gliderlabs/logspout/httpstream"
+	_ "github.com/gliderlabs/logspout/routesapi"
+	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
 )
